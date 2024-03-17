715a825fec24bc514509637b29eded29ffb0fff1
unknown <alidargahi1368@gmail.com>
2024-03-17 10:45:15 +0330

    .

--- app/section2/page.js
+++ app/section2/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="">
-     <Image src='/vercel.svg' width={100} height={100} alt=""/>
+     <Image src='/next.svg' width={100} height={100} alt=""/>
     </main>
   );
 }
