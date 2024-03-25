e17be3f17218c69dfff02a42d8b8778a67e338a5
ati-gh <atefehghoddusi@gmail.com>
2024-03-25 02:11:41 -0700

    section 2 design

--- app/section2/page.js
+++ app/section2/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import "./style.css";
 export default function Home() {
   return (
     <>
@@ -41,7 +42,7 @@ export default function Home() {
           </nav>
         </header>
       </section>
-      <section className="bg-[url('/images/Hero.png')] h-[702px]  mb-[200px] pt-[207px]">
+      <section className="bg-[url('/images/Hero.png')] h-[702px]  mb-[206px] pt-[207px]">
         <section className="mr-[98px] bg-[#00000066] w-[529px] h-[240px] rounded-[10px] backdrop-blur-[10px] p-12 text-white">
           <h2 className=" font-bold text-5xl mb-9">ایونتامین</h2>
           <p className="text-3xl leading-[42px]">
@@ -196,18 +197,101 @@ export default function Home() {
               </section>
 
               <section>
-                <button className=" flex gap-[10px] text-lg items-center transition ease-linear delay-500  hover:bg-gradient-to-r from-[#066A95] from-0% [#6D3A8E] [#6D3A8E]-45.6% to-[#B71789] text-white font-medium bg-[#066595] w-[166px] py-[14px] rounded-md">
+                <button className="btn relative flex gap-[10px] text-lg items-center text-white font-medium bg-[#066595] w-[166px] py-[14px] rounded-md ">
                   <Image
                     src="./assets/icons/search-normal.svg"
                     width={20.47}
                     height={20.47}
-                    className="mr-[40px]"
+                    className="mr-[40px] relative z-50"
                   />
-                  جستجو
+                  <span className="relative z-50">جستجو</span>
+                  <section className="section flex gap-[10px] items-center absolute left-0 h-full w-0 bg-gradient-to-r from-[#066A95] from-0% [#6D3A8E] [#6D3A8E]-45.6% to-[#B71789] rounded-md "></section>
                 </button>
               </section>
             </section>
           </section>
+        </section>
+      </section>
+      <section className="w-[1280px] mx-auto">
+        <section className="text-center">
+          <h2 className="text-[32px] font-bold mb-3 tracking-[-0.85px]">
+            با ایونتامین به راحتی هر رویدادی را برگزار کنید.
+          </h2>
+          <p className="text-lg font-medium mb-[35px]">
+            ایونتامین پلتفرم اختصاصی تامین کنندگان و برگزارکنندگان رویدادها و
+            نمایشگاه ها می باشد
+          </p>
+          <section className="flex justify-center gap-6 mb-16">
+            <button className="btn relative flex gap-[10px] text-lg items-center justify-center text-white font-medium bg-[#066595] px-[32px] py-[18px] rounded-md ">
+              <span className="relative z-50">
+                ثبت نام رایگان تامین کنندگان
+              </span>
+              <section className="section flex gap-[10px] items-center absolute left-0 h-full w-0 bg-gradient-to-r from-[#066A95] from-0% [#6D3A8E] [#6D3A8E]-45.6% to-[#B71789] rounded-md "></section>
+            </button>
+            <button className="btn relative flex gap-[10px] text-lg justify-center items-center text-white font-medium bg-[#066595] px-[32px] py-[18px] rounded-md ">
+              <span className="relative z-50">ثبت نام رایگان خریداران</span>
+              <section className="section flex gap-[10px] items-center absolute left-0 h-full w-0 bg-gradient-to-r from-[#066A95] from-0% [#6D3A8E] [#6D3A8E]-45.6% to-[#B71789] rounded-md "></section>
+            </button>
+          </section>
+        </section>
+        <section className="flex justify-center">
+          <Image
+            src="/assets/icons/right (1).svg"
+            width={66}
+            height={66}
+            className="mb-[90px]"
+          />
+          <section className="w-[728px]  ml-[-60px] mt-[50px] mb-[140px] shadow-[0px_16px_72px_0px_#0000003D] mr-4 rounded-lg ">
+            <ul className="w-[552px] h-[166px] justify-center my-[38px] mr-[45px] flex flex-col  list-disc">
+              <li>
+                شما به عنوان برگزارکننده می توانید با تامین کنندگان بدون واسطه
+                ارتباط برقرار کنید و با فرستادن درخواست بطور همزمان به چندین
+                تامین کننده، امکان مقایسه قیمت و خدمات درخواستی خود را دارید.
+              </li>
+              <li>
+                برای برگزاری هر رویدادی (جشن تولد، نمایشگاه، جلسه کاری و ....)
+                می توانید از طریق این پلتفرم، خدمات مورد نیاز خود را در هر کشور
+                و شهری، از تامین کنندگان معتبر دریافت کنید.
+              </li>
+            </ul>
+          </section>
+
+          <section className="flex items-center  ">
+            <Image
+              src="/images/1 (4).png"
+              width={321}
+              height={470}
+              className="z-40 ml-[-230px]"
+            />
+
+            <Image
+              src="/images/2 (1).png"
+              width={273.82}
+              height={354}
+              className=" z-30 ml-[-175px]"
+            />
+
+            <Image
+              src="/images/3 (1).png"
+              width={216.58}
+              height={280}
+              className=""
+            />
+          </section>
+          {/* <Image
+            src="/images/slider.png"
+            width={448}
+            height={415}
+            className=""
+          /> */}
+          <button>
+            <Image
+              src="/assets/icons/Subtract.svg"
+              width={52.8}
+              height={52.8}
+              className="mb-[40px]"
+            />
+          </button>
         </section>
       </section>
     </>
